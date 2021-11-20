f5d25fe036f8dd8442aa4b587625c041ef1c9e17
JAESKAAA <jaeskaaa@gmail.com>
2021-11-20 19:34:31 +0900

    EL / JSTL 연습 / Filter 실습

---
+++ src/main/java/com/fastcampus/ch2/PerformanceFilter.java
@@ -0,0 +1,41 @@
+package com.fastcampus.ch2;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+@WebFilter(urlPatterns="/*")
+public class PerformanceFilter implements Filter{
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		//초기화 작업
+	}
+	
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		//1. 전처리 작업
+		long startTime = System.currentTimeMillis();
+		
+		//2. 서블릿 또는 다음 필터 호출
+		chain.doFilter(request, response);
+		
+		//3. 후처리 작업
+		System.out.print("["+((HttpServletRequest)request).getRequestURI()+"]");
+		System.out.println(" 소요시간 = "+(System.currentTimeMillis()-startTime)+"ms");
+	}
+	@Override
+	public void destroy() {
+		//정리작업
+	}
+	
+}
